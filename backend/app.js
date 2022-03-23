c2930f4728cc856eb561d05ae8667b6b8d4f94b9
Alexander Harvey <harvey.alex01@gmail.com>
2022-03-23 11:25:30 +1100

    added heroes route that returns results that start with

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const logger = require('morganconst')
+const logger = require('morgan')
 const app = express()
+
+const results = require('./utils/mockResults')
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -11,9 +13,16 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
+app.get('/heroes', (req, res) => {
+  // Normally with a database attached, you would search the the database and return what you want,
+  // In this case we will just regex the results array
+  const hero = req.body.nameStartsWith
+  const regex = new RegExp(`^${hero}`)
+  const regexedResults = results.data.results.filter((result) =>
+    result.name.match(regex)
+  )
+  console.log('Filtred Results', regexedResults)
+  res.send(regexedResults)
 })
 
 module.exports = app
